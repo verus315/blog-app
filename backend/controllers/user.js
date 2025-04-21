96db70136d35322bb28c1fa0c55ee6fc665110be
ahasnat24 <ahasnat24@students.oamk.fi>
2025-04-21 16:23:58 +0600

    modified:   backend/controllers/user.js

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -6,7 +6,9 @@ const Post = require('../models/Post');
 // @access  Public
 exports.getUser = async (req, res) => {
   try {
-    const user = await User.findById(req.params.id).select('-password');
+    const user = await User.findByPk(req.params.id, {
+      attributes: { exclude: ['password'] }
+    });
     
     if (!user) {
       return res.status(404).json({
@@ -32,7 +34,7 @@ exports.getUser = async (req, res) => {
 // @access  Private
 exports.updateUser = async (req, res) => {
   try {
-    const user = await User.findById(req.params.id);
+    const user = await User.findByPk(req.params.id);
 
     if (!user) {
       return res.status(404).json({
@@ -42,109 +44,9 @@ exports.updateUser = async (req, res) => {
     }
 
     // Make sure user is updating their own profile or is admin
-    if (user._id.toString() !== req.user.id && req.user.role !== 'admin') {
+    if (user.id !== req.user.id && req.user.role !== 'admin') {
       return res.status(401).json({
         success: false,
         message: 'Not authorized to update this user'
       });
     }
-
-    // Update fields
-    user.name = req.body.name || user.name;
-    user.email = req.body.email || user.email;
-    if (req.body.avatar) user.avatar = req.body.avatar;
-
-    await user.save();
-
-    res.status(200).json({
-      success: true,
-      data: user
-    });
-  } catch (error) {
-    res.status(400).json({
-      success: false,
-      message: error.message
-    });
-  }
-};
-
-// @desc    Get all users (admin only)
-// @route   GET /api/v1/users
-// @access  Private/Admin
-exports.getUsers = async (req, res) => {
-  try {
-    const users = await User.find().select('-password');
-
-    res.status(200).json({
-      success: true,
-      count: users.length,
-      data: users
-    });
-  } catch (error) {
-    res.status(400).json({
-      success: false,
-      message: error.message
-    });
-  }
-};
-
-// @desc    Update user status (admin only)
-// @route   PUT /api/v1/users/:id/status
-// @access  Private/Admin
-exports.updateUserStatus = async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-
-    if (!user) {
-      return res.status(404).json({
-        success: false,
-        message: 'User not found'
-      });
-    }
-
-    user.isApproved = req.body.isApproved;
-    await user.save();
-
-    res.status(200).json({
-      success: true,
-      data: user
-    });
-  } catch (error) {
-    res.status(400).json({
-      success: false,
-      message: error.message
-    });
-  }
-};
-
-// @desc    Delete user (admin only)
-// @route   DELETE /api/v1/users/:id
-// @access  Private/Admin
-exports.deleteUser = async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-
-    if (!user) {
-      return res.status(404).json({
-        success: false,
-        message: 'User not found'
-      });
-    }
-
-    // Delete all posts by this user
-    await Post.deleteMany({ author: user._id });
-
-    // Delete the user
-    await user.remove();
-
-    res.status(200).json({
-      success: true,
-      message: 'User deleted successfully'
-    });
-  } catch (error) {
-    res.status(400).json({
-      success: false,
-      message: error.message
-    });
-  }
-}; 
